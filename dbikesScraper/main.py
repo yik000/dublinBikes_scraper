17da4b71c0e74e027df834db52a7de3e0ad5834e
Conor Kiy <conorkiy@gmail.com>
2021-02-20 15:58:13 +0000

    import packages

---
+++ dbikesScraper/main.py
@@ -0,0 +1,15 @@
+"""
+Created on 19 Feb 2021
+
+@author: conorkiy@gmail.com
+"""
+
+import dbinfo
+import datetime
+from datetime import datetime
+import requests
+import time
+import json
+import calendar
+from sqlalchemy import create_engine
+import traceback
