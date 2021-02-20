93e799185fa4aa34fde5b66bc3ac2ae12b21de04
Conor Kiy <conorkiy@gmail.com>
2021-02-20 16:09:28 +0000

    implement main function and sql insert function

--- dbikesScraper/main.py
+++ dbikesScraper/main.py
@@ -26,3 +26,41 @@ STATIONS="https://api.jcdecaux.com/vls/v1/stations?"
 create engine - connect to database
 """
 engine = create_engine("mysql+mysqldb://{}:{}@{}:3306/dbikes".format(dbinfo.USER, dbinfo.PASS, dbinfo.URI), echo=True)
+
+
+"""
+main
+"""
+def main():
+    while True:
+        try:
+
+            api_request = requests.get(STATIONS, params={"contract":NAME, "apiKey": dbinfo.APIKEY})
+
+            insert_into_availability(api_request.text)
+
+            time.sleep(5*60)
+
+        except:
+
+            print(traceback.format_exc())
+    
+    return
+
+
+"""
+insert into availability
+"""
+def insert_into_availability(text):
+
+    stations = json.loads(text)
+
+    for station in stations:
+        
+        now = datetime.fromtimestamp( int(station.get("last_update")/ 1e3) )
+        today = now.weekday()
+        day = calendar.day_name[today]
+        time = now.strftime("%H:%M")
+        values = (station.get("number"), station.get("available_bike_stands"), station.get("available_bikes"), station.get("status"), day, time)
+        
+        engine.execute("INSERT INTO availability VALUES(%s,%s,%s,%s,%s,%s)", values) 
