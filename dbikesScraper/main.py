67a90abbb88a567717c3076103c7f962e6c8856f
Conor Kiy <conorkiy@gmail.com>
2021-02-20 16:04:34 +0000

    create API variables and sql engine

--- dbikesScraper/main.py
+++ dbikesScraper/main.py
@@ -13,3 +13,16 @@ import json
 import calendar
 from sqlalchemy import create_engine
 import traceback
+
+
+"""
+API variables
+"""
+NAME="Dublin"
+STATIONS="https://api.jcdecaux.com/vls/v1/stations?"
+
+
+"""
+create engine - connect to database
+"""
+engine = create_engine("mysql+mysqldb://{}:{}@{}:3306/dbikes".format(dbinfo.USER, dbinfo.PASS, dbinfo.URI), echo=True)
