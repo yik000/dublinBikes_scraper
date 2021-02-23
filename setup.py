f8d97998fdc1592431df34cfe9376d76f904c5a1
Conor Kiy <conorkiy@gmail.com>
2021-02-23 17:57:35 +0000

    REFACTOR: update install_requires

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name="dbikes_scraper",
-    version="0.1",
+    version="1.0",
     description="Data scraper for dublin bikes API",
     author="Conor Kiy",
     author_email="conorkiy@gmail.com",
     licence = "GPL3",
-    install_requires=['requests','sqlalchemy','mysqlclient'],
+    install_requires=['requests','sqlalchemy','mysql-connector-python'],
     packages=['dbikesScraper'],
     entry_points={
         'console_scripts':['dbikes_scraper=dbikesScraper.main:main']
